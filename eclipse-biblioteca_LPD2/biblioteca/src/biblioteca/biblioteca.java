d458d5c326ff33ea49298b0f29698620370c83e1
carolvfq <carolvfdq@gmail.com>
2025-08-21 21:49:57 -0300

    primeiro commit

---
+++ eclipse-biblioteca_LPD2/biblioteca/src/biblioteca/biblioteca.java
@@ -0,0 +1,11 @@
+package biblioteca;
+
+public class biblioteca {
+
+	public static void main(String[] args) {
+		
+    livro livro = new livro("LPII", "thiago",  2025);
+    System.out.println(livro);
+	}
+
+}

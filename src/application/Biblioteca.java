2854aaabc2d2a9b4606037aa9b4f366fe0e6a955
carolvfq <carolvfdq@gmail.com>
2025-09-02 14:53:48 -0300

    atualizando projeto

---
+++ src/application/Biblioteca.java
@@ -0,0 +1,109 @@
+
+package application;
+
+import entities.Emprestimo;
+import entities.Livro;
+import entities.Usuario;
+import enums.StatusLivro;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca {
+
+    private List<Livro> acervo;
+    private List<Usuario> listaDeUsuarios;
+    private List<Emprestimo> registroDeEmprestimos;
+
+    public Biblioteca() {
+        this.acervo = new ArrayList<>();
+        this.listaDeUsuarios = new ArrayList<>();
+        this.registroDeEmprestimos = new ArrayList<>();
+    }
+
+    public void realizarEmprestimo(String idUsuario, String titulo) {
+        // 1 - Buscar os objetos usuario e livro
+        Usuario usuarioDoEmprestimo = pesquisarUsuarioPorId(idUsuario);
+        if(usuarioDoEmprestimo == null) {
+            System.out.println("Erro: esse usuário não está cadastrado.");
+            return;
+        }
+        Livro livroDoEmprestimo = pesquisarLivroPorTitulo(titulo);
+        if(livroDoEmprestimo == null) {
+            System.out.println("Erro: esse livro não está cadastrado.");
+            return;
+        }
+        // 2 - Validar a regra de negocio (verificar se o livro está disponível)
+        if(livroDoEmprestimo.getStatus() == StatusLivro.EMPRESTADO) {
+            System.out.println("Erro: esse livro já está emprestado.");
+            return;
+        }
+        // 3 - Realizar o registro (Criar objeto do tipo Emprestimo e adiciona-lo ao registroDeEmprestimos)
+        livroDoEmprestimo.setStatus(StatusLivro.EMPRESTADO);
+        Emprestimo emprestimo = new Emprestimo(livroDoEmprestimo, usuarioDoEmprestimo, LocalDate.now());
+        registroDeEmprestimos.add(emprestimo);
+        System.out.println("Emprestido cadastrado com sucesso.");
+    }
+
+    public Livro pesquisarLivroPorTitulo(String titulo) {
+        for(Livro livro : this.acervo) {
+            if(livro.getTitulo().equalsIgnoreCase(titulo)) {
+                return livro;
+            }
+        }
+        return null;
+    }
+
+    public Usuario pesquisarUsuarioPorId(String id) {
+        for(Usuario usuario : this.listaDeUsuarios) {
+            if(usuario.getId().equals(id)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    public List<Livro> pesquisarLivroPorTermo(String termo) {
+        List<Livro> listaDeLivros = new ArrayList<>();
+        for (var livro : acervo) {
+            if(livro.getTitulo().toLowerCase().contains(termo.toLowerCase())) {
+                listaDeLivros.add(livro);
+            }
+        }
+        return listaDeLivros;
+    }
+
+    public void listarAcervo() {
+        System.out.println("Livros no Acervo");
+        for (Livro livro : acervo) {
+            System.out.println(livro);
+        }
+    }
+
+    public void cadastrarLivro(Livro livro) {
+        this.acervo.add(livro);
+        System.out.println("O livro " + livro.getTitulo() + " foi cadastrado.");
+    }
+
+    public void cadastrarUsuario(Usuario usuario) {
+        this.listaDeUsuarios.add(usuario);
+        System.out.println("O usuário " + usuario.getNome() + " foi cadastrado.");
+    }
+
+    public static void main(String[] args) {
+        Livro livroJavaComoProgramar = new Livro("Java Como Programar", "Deitel", 2014);
+        Livro livroMemoria = new Livro("Memórias Póstumas de Brás Cubas", "Machado de Assis", 1881);
+        Usuario meuUsuario = new Usuario("Thiago", "123");
+        Biblioteca minhaBiblioteca = new Biblioteca();
+        minhaBiblioteca.cadastrarLivro(livroJavaComoProgramar);
+        minhaBiblioteca.cadastrarLivro(livroMemoria);
+        minhaBiblioteca.cadastrarUsuario(meuUsuario);
+        List<Livro> resultado = minhaBiblioteca.pesquisarLivroPorTermo("programação");
+        for (var livro : resultado) {
+            System.out.println("Livros encontrados");
+            System.out.println(livro);
+        }
+        minhaBiblioteca.listarAcervo();
+    }
+}
---
+++ src/entities/Livro.java
@@ -0,0 +1,68 @@
+package entities;
+
+import enums.StatusLivro;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int ano;
+    private StatusLivro status;
+
+    public Livro(String titulo, String autor, int ano) {
+        setTitulo(titulo);
+        setAutor(autor);
+        setAno(ano);
+        this.status = StatusLivro.DISPONIVEL;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        int ano_atual = 2025;
+        if (ano > ano_atual) {
+            System.out.println("Erro: ano inválido.");
+        } else {
+            this.ano = ano;
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        if (titulo == "") {
+            System.out.println("Erro: título inválido.");
+        } else {
+            this.titulo = titulo;
+        }
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        if (autor == "") {
+            System.out.println("Erro: título inválido.");
+        } else {
+            this.autor = autor;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Livro '" + titulo + "', de " + autor + " (" + ano + ") - Status: " + status;
+    }
+
+    public StatusLivro getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusLivro status) {
+        this.status = status;
+    }
+}
+
